b33e90de0ebf73e531fbc2408c61309cb9b1b90d
R git config --global user.name R <958844609@qq.com>
2017-04-26 19:29:07 +0800

    公告小改动

---
+++ src/main/java/com/questionnaire/ssm/module/notice/pojo/DisplayNoticeVO.java
@@ -0,0 +1,7 @@
+package com.questionnaire.ssm.module.notice.pojo;
+
+/**
+ * Created by 95884 on 2017/4/19.
+ */
+public class DisplayNoticeVO {
+}
---
+++ src/main/java/com/questionnaire/ssm/module/notice/pojo/NoticeUnitVO.java
@@ -0,0 +1,25 @@
+package com.questionnaire.ssm.module.notice.pojo;
+
+/**
+ * Created by 95884 on 2017/4/19.
+ */
+public class NoticeUnitVO {
+    private int unitOrder;
+    private String Unit;
+
+    public int getUnitOrder() {
+        return unitOrder;
+    }
+
+    public void setUnitOrder(int unitOrder) {
+        this.unitOrder = unitOrder;
+    }
+
+    public String getUnit() {
+        return Unit;
+    }
+
+    public void setUnit(String unit) {
+        Unit = unit;
+    }
+}
