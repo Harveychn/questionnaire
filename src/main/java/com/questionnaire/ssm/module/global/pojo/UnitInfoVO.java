893778763b3b5be98a29de625d215205b0f9f96b
Harveychn <zxh980278090@gmail.com>
2017-04-27 10:13:37 +0800

    创建调查页面

---
+++ src/main/java/com/questionnaire/ssm/module/global/pojo/UnitInfoVO.java
@@ -0,0 +1,19 @@
+package com.questionnaire.ssm.module.global.pojo;
+
+/**
+ * Created by 郑晓辉 on 2017/4/27.
+ * Description: 单位信息视图实体类
+ */
+public class UnitInfoVO {
+    //单位ID
+    private Long unitId;
+    //单位名
+    private String unitName;
+    //单位编号
+    private String unitCode;
+    //单位所在省份
+    private String unitProvince;
+    //单位所在城市
+    private String unitCity;
+
+}
