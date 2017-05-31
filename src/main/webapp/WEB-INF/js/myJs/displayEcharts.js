aef7c3e2e2ea570f683f978f8bae6a44ca55461d
Harveychn <zxh980278090@gmail.com>
2017-05-31 13:43:10 +0800

    结果可视化 --完成

--- src/main/webapp/WEB-INF/js/myJs/displayEcharts.js
+++ src/main/webapp/WEB-INF/js/myJs/displayEcharts.js
@@ -5,36 +5,57 @@
 var dom = document.getElementById("echartsArea");
 var myChart = echarts.init(dom);
 var app = {};
-// option = null;
 
-function fetchData(cb) {
-    // 通过 setTimeout 模拟异步加载
-    setTimeout(function () {
-        //后台数据格式
-        cb({
-            categories: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子", "测试"],
-            data: [5, 20, 36, 10, 10, 20, 21]
-        });
-    }, 1000);
+var $curQuestionContent = $('#curQuestionContent');
+var $questionList = $('#questionList');
+var $tableBody = $('#tableBody');
+var $questionTableData = $('#questionTableData');
+
+//当前问卷结果分析数据
+var analyzeResultData;
+
+$(function () {
+    //初始化时样式
+    //显示加载中样式
+    myChart.showLoading();
 
     $.ajax({
         url: '/statisticalAnalysis/getQesPaperAnalyzeResult',
         type: 'get',
+        async: false,
         dataType: 'json',
         success: function (data) {
-            console.log(data);
+            analyzeResultData = data;
         },
         error: function (data) {
             console.log(data);
         }
     });
-}
 
-//初始化时样式
-//显示加载中样式
-myChart.showLoading();
-//设置数据
-fetchData(function (data) {
+    for (var i = 0; i < analyzeResultData.length; i++) {
+        $questionList.append('<li class="list-group-item btn" style="text-align: left;" id="' + analyzeResultData[i].questionId
+            + '" onclick="clickQuestionListItem(this.id)">第'
+            + (i + 1) + '题  ' + analyzeResultData[i].questionContent + '(' + analyzeResultData[i].questionType + ')</li>');
+        if (i === 0) {
+            $curQuestionContent.html('第'
+                + (i + 1) + '题  ' + analyzeResultData[i].questionContent + '(' + analyzeResultData[i].questionType + ')');
+            //设置表格的数据
+            setTableBodyData(i);
+        }
+    }
+
+    initEchartData();
+});
+
+var curQuestionAnalyzeData = {};
+
+/**
+ * 初始化展示Echarts视图
+ */
+function initEchartData() {
+    var initData = analyzeResultData[0];
+    curQuestionAnalyzeData.categories = initData.optionList;
+    curQuestionAnalyzeData.data = initData.valueList;
     myChart.hideLoading();
     var initOption = {
         tooltip: {
@@ -72,217 +93,306 @@ fetchData(function (data) {
         yAxis: {
             type: 'category',
             //动态数据
-            data: data.categories
+            data: initData.optionList
         },
         series: [
             //动态数据
             {
                 type: 'bar',
-                data: data.data
+                data: initData.valueList
             }
         ]
     };
-    myChart.setOption(initOption);
-});
+    setEchartOption(initOption);
+}
+
+
+function clickQuestionListItem(qesId) {
+    for (var i = 0; i < analyzeResultData.length; i++) {
+        if (analyzeResultData[i].questionId == qesId) {
+            curQuestionAnalyzeData.categories = analyzeResultData[i].optionList;
+            curQuestionAnalyzeData.data = analyzeResultData[i].valueList;
+            $curQuestionContent.html('第'
+                + (i + 1) + '题  ' + analyzeResultData[i].questionContent + '(' + analyzeResultData[i].questionType + ')');
+
+            //设置表格数据
+            setTableBodyData(i);
+        }
+    }
+    dynamicDataChange(curQuestionAnalyzeData);
+}
+
+function setTableBodyData(i) {
+    // 表格数据初始化处理
+    $tableBody.empty();
+    var optionList = analyzeResultData[i].optionList;
+    var valueList = analyzeResultData[i].valueList;
+    var sumValue = 0;
+    var rowDataArray = [];
+    for (var index = 0; index < valueList.length; index++) {
+        sumValue += valueList[index];
+        var tableRowData = {};
+        tableRowData.option = optionList[index];
+        tableRowData.value = valueList[index];
+        rowDataArray.push(tableRowData);
+    }
+    for (var rowindex = 0; rowindex < rowDataArray.length; rowindex++) {
+        if (sumValue === 0) {
+            $tableBody.append('<tr>' +
+                '<td>' + rowDataArray[rowindex].option + '</td>' +
+                '<td>' + rowDataArray[rowindex].value + '</td>' +
+                '<td>0%</td>' +
+                '</tr>');
+        } else {
+            $tableBody.append('<tr>' +
+                '<td>' + rowDataArray[rowindex].option + '</td>' +
+                '<td>' + rowDataArray[rowindex].value + '</td>' +
+                '<td>' + rowDataArray[rowindex].value / sumValue * 100 + '%</td>' +
+                '</tr>');
+        }
+    }
+}
+
+function dynamicDataChange(dynamicDataChange) {
+    var dynamicOption = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        toolbox: {
+            show: true,
+            orient: 'horizontal',
+            feature: {
+                saveAsImage: {
+                    show: true
+                },
+                restore: {
+                    show: true
+                },
+                dataZoom: {
+                    show: true
+                }
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            position: 'top',
+            type: 'value',
+            boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+            type: 'category',
+            //动态数据
+            data: dynamicDataChange.categories
+        },
+        series: [
+            //动态数据
+            {
+                type: 'bar',
+                data: dynamicDataChange.data
+            }
+        ]
+    };
+    setEchartOption(dynamicOption);
+}
 
 //点击表格按钮后样式
 $('#tableBtn').on('click', function () {
+    if($questionTableData.attr('hidden')===undefined){
+        $questionTableData.attr('hidden','hidden');
+    }else {
+        $questionTableData.removeAttr('hidden');
+    }
 });
 //点击柱状图
 $('#histogramBtn').on('click', function () {
     myChart.showLoading();
     //设置数据
-    fetchData(function (data) {
-        myChart.hideLoading();
-        //柱状图 option
-        var histogramOption = {
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow'
+    myChart.hideLoading();
+    //柱状图 option
+    var histogramOption = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        toolbox: {
+            show: true,
+            orient: 'horizontal',
+            feature: {
+                saveAsImage: {
+                    show: true
+                },
+                restore: {
+                    show: true
+                },
+                dataZoom: {
+                    show: true
                 }
-            },
-            toolbox: {
-                show: true,
-                orient: 'horizontal',
-                feature: {
-                    saveAsImage: {
-                        show: true
-                    },
-                    restore: {
-                        show: true
-                    },
-                    dataZoom: {
-                        show: true
-                    }
-                }
-            },
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: {
-                type: 'category',
-                //动态数据
-                // data: []
-                data: data.categories
-            },
-            yAxis: {
-                position: 'top',
-                type: 'value',
-                boundaryGap: [0, 0.01]
-            },
-            series: [//动态数据
-                {
-                    type: 'bar',
-                    data: data.data
-                }
-            ]
-        };
-        setEchartOption(histogramOption);
-    });
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            //动态数据
+            // data: []
+            data: curQuestionAnalyzeData.categories
+        },
+        yAxis: {
+            position: 'top',
+            type: 'value',
+            boundaryGap: [0, 0.01]
+        },
+        series: [//动态数据
+            {
+                type: 'bar',
+                data: curQuestionAnalyzeData.data
+            }
+        ]
+    };
+    setEchartOption(histogramOption);
 });
 
 //点击条形图后样式
 $('#barGraphBtn').on('click', function () {
     myChart.showLoading();
-    fetchData(function (data) {
-        myChart.hideLoading();
-        //条形图 option
-        var barGraphOption = {
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow'
+    myChart.hideLoading();
+    //条形图 option
+    var barGraphOption = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        }, toolbox: {
+            show: true,
+            orient: 'horizontal',
+            feature: {
+                saveAsImage: {
+                    show: true
+                },
+                restore: {
+                    show: true
+                },
+                dataZoom: {
+                    show: true
                 }
-            }, toolbox: {
-                show: true,
-                orient: 'horizontal',
-                feature: {
-                    saveAsImage: {
-                        show: true
-                    },
-                    restore: {
-                        show: true
-                    },
-                    dataZoom: {
-                        show: true
-                    }
-                }
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left',
-                //动态数据
-                // data: []
-                data: data.categories
-            },
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: {
-                position: 'top',
-                type: 'value',
-                boundaryGap: [0, 0.01]
-            },
-            yAxis: {
-                type: 'category',
-                //动态数据
-                // data: []
-                data: data.categories
-            },
-            series: [//动态数据
-                {
-                    type: 'bar',
-                    data: data.data
-                }
-            ]
-        };
-        setEchartOption(barGraphOption);
-    });
+            }
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            //动态数据
+            // data: []
+            data: curQuestionAnalyzeData.categories
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: {
+            position: 'top',
+            type: 'value',
+            boundaryGap: [0, 0.01]
+        },
+        yAxis: {
+            type: 'category',
+            //动态数据
+            // data: []
+            data: curQuestionAnalyzeData.categories
+        },
+        series: [//动态数据
+            {
+                type: 'bar',
+                data: curQuestionAnalyzeData.data
+            }
+        ]
+    };
+    setEchartOption(barGraphOption);
 });
 
 //点击扇形图后样式
 $('#pieChartBtn').on('click', function () {
     myChart.showLoading();
-    fetchData(function (data) {
-        var resultData = [];
-        var dataLength = data.data.length;
-        for (var i = 0; i < dataLength; i++) {
-            var dataItem = {};
-            dataItem.name = data.categories[i];
-            dataItem.value = data.data[i];
-            resultData.push(dataItem);
-        }
-
-        console.log(resultData);
-
-        myChart.hideLoading();
-        //扇形图 option
-        var pieChartOption = {
-            // title: {
-            //     text: '某站点用户访问来源',
-            //     subtext: '纯属虚构',
-            //     x: 'center'
-            // },
-            tooltip: {
-                trigger: 'item',
-                formatter: "{a} <br/>{b} : {c} ({d}%)"
-            }, toolbox: {
-                show: true,
-                orient: 'horizontal',
-                feature: {
-                    saveAsImage: {
-                        show: true
-                    },
-                    restore: {
-                        show: true
-                    },
-                    dataZoom: {
-                        show: true
-                    }
+    var resultData = [];
+    var dataLength = curQuestionAnalyzeData.data.length;
+    for (var i = 0; i < dataLength; i++) {
+        var dataItem = {};
+        dataItem.name = curQuestionAnalyzeData.categories[i];
+        dataItem.value = curQuestionAnalyzeData.data[i];
+        resultData.push(dataItem);
+    }
+    myChart.hideLoading();
+    //扇形图 option
+    var pieChartOption = {
+        // title: {
+        //     text: '某站点用户访问来源',
+        //     subtext: '纯属虚构',
+        //     x: 'center'
+        // },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        }, toolbox: {
+            show: true,
+            orient: 'horizontal',
+            feature: {
+                saveAsImage: {
+                    show: true
+                },
+                restore: {
+                    show: true
+                },
+                dataZoom: {
+                    show: true
                 }
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left',
+            }
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            right: '20%',
+            // itemWidth:'20',
+            // itemHeight:'10',
+            //动态数据
+            // data: []
+            data: curQuestionAnalyzeData.categories
+        },
+        series: [
+            {
+                name: '比例',
+                // name: '访问来源',
+                type: 'pie',
+                radius: '40%',
+                center: ['60%', '60%'],
                 //动态数据
-                // data: []
-                data: data.categories
-            },
-            series: [
-                {
-                    name: '比例',
-                    // name: '访问来源',
-                    type: 'pie',
-                    radius: '55%',
-                    center: ['50%', '60%'],
-                    //动态数据
-                    data: resultData,
-                    // data: [
-                    //     {value: 335, name: '直接访问'},
-                    //     {value: 310, name: '邮件营销'},
-                    //     {value: 234, name: '联盟广告'},
-                    //     {value: 135, name: '视频广告'},
-                    //     {value: 1548, name: '搜索引擎'}
-                    // ],
-                    itemStyle: {
-                        emphasis: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
+                data: resultData,
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
                     }
                 }
-            ]
-        };
-        setEchartOption(pieChartOption);
-    });
+            }
+        ]
+    };
+    setEchartOption(pieChartOption);
 });
 
 
