11111b9e320b96b826fefac366b2bbc5700cb143
Harveychn <zxh980278090@gmail.com>
2017-07-05 19:51:59 +0800

    导出表格数据为xlsx、xls

--- src/main/webapp/WEB-INF/js/plugins/table-export/table-export.js
+++ src/main/webapp/WEB-INF/js/plugins/table-export/table-export.js
@@ -1,8 +1,12 @@
 //为导出功能设置数据源
 function setExportDataAttr() {
-    $(".export-png").attr("data-table","#questionTableData");
+    $(".export-png").attr("data-table", "#questionTableData");
+    $(".export-xls").attr("data-table", "#questionTableData");
+    $(".export-xlsx").attr("data-table", "#questionTableData");
 
-    $(".export-png").attr("data-filename","导出数据");
+    $(".export-png").attr("data-filename", "图片数据");
+    $(".export-xls").attr("data-filename", "Excel数据");
+    $(".export-xlsx").attr("data-filename", "Excel数据");
 };
 
 var TableExport = function () {
@@ -17,6 +21,31 @@ var TableExport = function () {
             $(exportTable).tableExport({
                 fileName: filename,
                 type: 'png'
+            });
+        });
+        $(".export-xls").on("click", function (e) {
+            e.preventDefault();
+            var exportTable = $(this).data("table");
+            var filename = $(this).data("filename");
+            var ignoreColumn = $(this).data("ignorecolumn");
+            $(exportTable).tableExport({
+                fileName: filename,
+                type: 'excel',
+                escape: 'false',
+                excelstyles: ['border-bottom', 'border-top', 'border-left', 'border-right'],
+                ignoreColumn: '[' + ignoreColumn + ']'
+            });
+        });
+        $(".export-xlsx").on("click", function (e) {
+            e.preventDefault();
+            var exportTable = $(this).data("table");
+            var filename = $(this).data("filename");
+            var ignoreColumn = $(this).data("ignorecolumn");
+            $(exportTable).tableExport({
+                fileName: filename,
+                type: 'xlsx',
+                escape: 'false',
+                ignoreColumn: '[' + ignoreColumn + ']'
             });
         });
     };
